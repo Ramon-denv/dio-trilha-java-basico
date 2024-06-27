448aa5ac4efc717e2adcc6726dd4ce0c6284d18e
Ramon-denv <ramonmendespeixoto008@gmail.com>
2024-06-26 22:02:41 -0300

    Padrão de projeto

---
+++ padrão de projeto Java/src/App.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        List<Aluno> listaAlunos = new ArrayList<>();
+
+        
+        
+        Aluno aluno = new Aluno("Ramon", 10, 134964796);
+
+        System.out.println(aluno);
+
+
+
+    }
+}
